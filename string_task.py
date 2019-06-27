a143eee85df561a2282abfc086f67ab55b8e18b5
moeabdal <mohammed.h.abdal@gmail.com>
2019-06-27 18:15:16 +0300

    Removed tripple quotes.

--- string_task.py
+++ string_task.py
@@ -6,8 +6,4 @@ time = input("Please enter a number between 1 and 12: ")
 scream = input("Please enter a random sentence: ").upper()
 action = input("Please enter a verb: ")
 
-print ("""It was %s o'clock when I heard a knock on the door.
-I opened the door and and there was a box full of %s, with a note saying "From %s".
-Just as I closed the door, someone screamed, "%s".
-I froze in place and all I could do was %s.
-""") % (time, item, name, scream, action)
+print ("It was %s o'clock when I heard a knock on the door. I opened the door and and there was a box full of %s, with a note saying 'From %s'. Just as I closed the door, someone screamed, '%s'. I froze in place and all I could do was %s.") % (time, item, name, scream, action)
