7912220e44ce2c2375251e4f969185a46ea7b60a
moeabdal <mohammed.h.abdal@gmail.com>
2019-06-27 17:59:18 +0300

    Fixed syntax error.

--- string_task.py
+++ string_task.py
@@ -6,8 +6,8 @@ time = input("Please enter a number between 1 and 12: ")
 scream = input("Please enter a random sentence: ").upper()
 action = input("Please enter a verb: ")
 
-print """It was %s o'clock when I heard a knock on the door.
+print ("""It was %s o'clock when I heard a knock on the door.
 I opened the door and and there was a box full of %s, with a note saying "From %s".
-Just as I closed the door, somone screamed, "%s".
+Just as I closed the door, someone screamed, "%s".
 I froze in place and all I could do was %s.
-""" % (time, item, name, scream, action)
+""") % (time, item, name, scream, action)
