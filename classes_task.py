5374502f3935fdae73f97a6a1ea56ff8b42b3ec8
moeabdal <mohammed.h.abdal@gmail.com>
2019-07-03 23:21:44 +0300

    Finished classes task.

--- classes_task.py
+++ classes_task.py
@@ -1,66 +1,85 @@
 import datetime
 today = datetime.date.today()
 
-class Employee:
+class Employee(object):
 	def __init__(self, name, age, salary, employment_date):
-		self.name = input("Enter name: ")
-		self. age = input("Enter age: ")
-		self.salary = input("Enter salary: ")
-		self.employment_date = ("Enter employment year: ")
-	def get_working_years(self):
-		working_years = today.year - employment_date
-		return working_years
-	def __str__(self):
-		return Employee("Name: %s, Age: %s, Salary: %s, Working years: %s" % (self.name, self.age, self.salary, working_years))
-
-class Manager:
-	def __init__(self, name, age, salary, employment_date, bonus_percentage):
 		self.name = name
 		self.age = age
-		self.salary = salary
-		self.employment_date = employment_date
-		self.bonus_percentage = bonus_percentage
+		self.salary = float(salary)
+		self.employment_date = int(employment_date)
 	def get_working_years(self):
-		working_years = today.year - employment_date
+		working_years = today.year - self.employment_date
 		return working_years
+	def __repr__(self):
+		return "Name: %s, Age: %s, Salary: %.3fKD, Working years: %s" % (self.name.title(), self.age, self.salary, self.get_working_years())
+	
+
+class Manager(Employee):
+	def __init__(self, name, age, salary, employment_date, bonus_percentage):
+		Employee.__init__(self, name, age, salary, employment_date)
+		self.bonus_percentage = bonus_percentage
+	
 	def get_bonus(self):
-		bonus = bonus_percentage * salary
+		bonus = float(self.bonus_percentage) * int(self.salary)
 		return bonus
+	def __repr__(self):
+		return "Name: %s, Age: %s, Salary: %.3fKD, Working years: %s, Bonus: %.3fKD" % (self.name.title(), self.age, self.salary, self.get_working_years(), self.get_bonus())
 
 emplist = []
 manlist = []
 
-
-print ("Welcome to HR pro 2019")
-
+print (" ")
+print ("\tWelcome to HR pro 2019")
 
 
 while True:
-	print ("Choose one of the following options: \n\t 1. Show employees \n\t 2. Show managers \n\t 3. Add an employee \n\t 4. Add a manager \n\t 5. Exit")
+	print ("\tChoose one of the following options: \n\t\t 1. Show employees \n\t\t 2. Show managers \n\t\t 3. Add an employee \n\t\t 4. Add a manager \n\t\t 5. Exit")
 	action = input("\n What would you like to do? ")
+	
 	if action == "1":
 		if len(emplist) == 0:
-			print ("\n Please add employee first.\n")
+			print ("\n\t Please add employee first.\n")
 		else:
+			print (" ")
 			print (emplist)
+			print(" ")
+	
 	elif action == "2":
 		if len(manlist) == 0:
-			print ("\n Please add manager first.\n")
+			print ("\n\tPlease add manager first.\n")
 		else:
+			print (" ")
 			print (manlist)
+			print (" ")
+	
 	elif action == "3":
-		Employee()
-		print ("Employee added successfully.")
+		print (" ")
+		name = input("Enter name: ")
+		age = input("Enter age: ")
+		salary = input("Enter salary: ")
+		employment_date = input("Enter employment year: ")
+		empinst = Employee(name, age, salary, employment_date)
+		emplist.append(empinst)
+		
+
+		print ("\n\tEmployee added successfully.\n")
+	
 	elif action == "4":
-		input("Enter name: ").append(manlist)
-		input("Enter age: ").append(manlist)
-		input("Enter salary: ").append(manlist)
-		input("Enter year: ").date.year.append(manlist)
-		float(input("Enter bonus: ").append(manlist))
-		print ("Manager added successfully.")
+		print (" ")
+		name = input("Enter name: ")
+		age = input("Enter age: ")
+		salary = input("Enter salary: ")
+		employment_date = input("Enter employment year: ")
+		bonus_percentage = input("Enter bonus percentage: ")
+		maninst = Manager(name, age, salary, employment_date, bonus_percentage)
+		manlist.append(maninst)
+		
+		print ("\n\tManager added successfully.\n")
+	
 	elif action == "5":
+		print ("\n\tThank you for using HR pro 2019.\n")
 		break
 	else:
-		print ("Invalid input.")
+		print ("\n\tInvalid input.\n")
 
 
